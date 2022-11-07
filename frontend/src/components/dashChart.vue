e4143b8eb31b3955e0e17d5706a0135095f2f2f6
xlwillia <67807020+xlwillia@users.noreply.github.com>
2022-11-07 00:27:42 -0600

    GET data from backend on to frontend

--- frontend/src/components/dashChart.vue
+++ frontend/src/components/dashChart.vue
@@ -1,67 +1,84 @@
 <template>
-    <Bar
-      :chart-options="chartOptions"
-      :chart-data="chartData"
-      :chart-id="chartId"
-      :dataset-id-key="datasetIdKey"
-      :plugins="plugins"
-      :css-classes="cssClasses"
-      :styles="styles"
-      :width="width"
-      :height="height"
-    />
+  <Bar  :chart-data="chartData"/>
 </template>
   
 <script>
+  let apiURL = import.meta.env.VITE_ROOT_API + `/eventdata/dash/`;
   import { Bar } from 'vue-chartjs'
   import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  
+  import axios from 'axios'
+
   ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
   
-  export default {
-    name: 'BarChart',
-    components: { Bar },
-    props: {
-      chartId: {
-        type: String,
-        default: 'bar-chart'
-      },
-      datasetIdKey: {
-        type: String,
-        default: 'label'
-      },
-      width: {
-        type: Number,
-        default: 400
-      },
-      height: {
-        type: Number,
-        default: 400
-      },
-      cssClasses: {
-        default: '',
-        type: String
-      },
-      styles: {
-        type: Object,
-        default: () => {}
-      },
-      plugins: {
-        type: Object,
-        default: () => {}
-      }
-    },
-    data() {
-      return {
-        queryData: [],
-        chartData: {
-          labels: [ 'January', 'February', 'March' ],
-          datasets: [ { data: [40, 20, 12] } ]
-        },
-        chartOptions: {
-          responsive: true
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    function getNames () {
+      return axios.get(apiURL)
+      .then(resp => {
+        const eventNames = []
+        for (const key in resp.data) {
+        eventNames.push(resp.data[key].eventName)
         }
+        return eventNames
+      })
+      .catch(error => {
+      console.log(error);
+      return Promise.reject(error);
+      });
+    }
+    function getAttend () {
+      return axios.get(apiURL)
+      .then(resp => {
+        const numAttend = []
+        for (const key in resp.data) {
+        numAttend.push(resp.data[key].numberOfAttendees)
+        }
+        return numAttend
+      })
+      .catch(error => {
+      console.log(error);
+      return Promise.reject(error);
+      });
+    }
+    const eventNames = getNames();
+    const eventAttend = getAttend();
+    console.log(eventNames);
+    console.log(eventAttend);
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March'],
+        datasets: [
+          {
+            label: 'Number of Attendees',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12]
+          }
+        ]
       }
     }
   }
+}
+  /* export default {
+    name: 'BarChart',
+    components: { Bar },
+    data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+    try {
+      let apiURL = import.meta.env.VITE_ROOT_API + `/eventdata/dash/`;
+      axios.get(apiURL).then((resp) => {
+        this.chartdata = resp.data;
+        console.log(this.chartdata)
+      });
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  } */
 </script>
