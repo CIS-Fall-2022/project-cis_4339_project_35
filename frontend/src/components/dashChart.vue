031a591c9818a3fd71f5272c6d475b4eabdb8922
xlwillia <67807020+xlwillia@users.noreply.github.com>
2022-11-20 14:48:22 -0600

    Fixed commented out element

--- frontend/src/components/dashChart.vue
+++ frontend/src/components/dashChart.vue
@@ -96,38 +96,37 @@
       }
     }
   }
-// Vuechart.js Reference
-// <template>
-//  <div class="container">
-//   <Bar v-if="loaded" :chart-data="chartData" />
-//  </div>
-// </template>
+/* Vuechart.js Reference
+<template>
+ <div class="container">
+  <Bar v-if="loaded" :chart-data="chartData" />
+ </div>
+</template>
 
-//<script>
-//import { Bar } from 'vue-chartjs'
-//import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-//ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-//export default {
-//  name: 'BarChart',
-//  components: { Bar },
-//  data: () => ({
-//    loaded: false,
-//    chartData: null
-//  }),
-//  async mounted () {
-//    this.loaded = false
+export default {
+ name: 'BarChart',
+ components: { Bar },
+ data: () => ({
+   loaded: false,
+   chartData: null
+ }),
+ async mounted () {
+   this.loaded = false
 
-//    try {
-//      const { userlist } = await fetch('/api/userlist')
-//      this.chartdata = userlist
+   try {
+     const { userlist } = await fetch('/api/userlist')
+     this.chartdata = userlist
 
-//      this.loaded = true
-//    } catch (e) {
-//      console.error(e)
-//    }
-//  }
-//}
-//</script>
-//</script>
+     this.loaded = true
+   } catch (e) {
+     console.error(e)
+   }
+ }
+} */
+</script>
