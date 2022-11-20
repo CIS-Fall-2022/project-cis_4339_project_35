4f80e98b1747d15b71bc8c60140cf67f5d669ef4
xlwillia <67807020+xlwillia@users.noreply.github.com>
2022-11-20 09:04:19 -0600

    Update dashChart.vue

--- frontend/src/components/dashChart.vue
+++ frontend/src/components/dashChart.vue
@@ -96,21 +96,4 @@
       }
     }
   }
-  // Sample Code used from Chartjs.org to create our bar charts
-  // const config = {
-  // type: 'bar',
-  // data: data,
-  // options: {
-  //   responsive: true,
-  //   plugins: {
-  //     legend: {
-  //       position: 'top',
-  //     },
-  //     title: {
-  //       display: true,
-  //      text: 'Chart.js Bar Chart'
-  //    }
- //   }
-//  },
-// };
 </script>
