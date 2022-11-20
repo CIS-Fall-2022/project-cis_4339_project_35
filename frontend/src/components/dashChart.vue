7e8999b0dd7dd7a0a4aaf5522cc127cf791f67fe
Joshua Jaja <joshua16jaja@gmail.com>
2022-11-19 19:26:23 -0600

    Reference code used to create chart

--- frontend/src/components/dashChart.vue
+++ frontend/src/components/dashChart.vue
@@ -96,4 +96,21 @@
       }
     }
   }
+  // Sample Code used from Chartjs.org to create our bar charts
+  // const config = {
+  // type: 'bar',
+  // data: data,
+  // options: {
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       position: 'top',
+  //     },
+  //     title: {
+  //       display: true,
+  //      text: 'Chart.js Bar Chart'
+  //    }
+ //   }
+//  },
+// };
 </script>
